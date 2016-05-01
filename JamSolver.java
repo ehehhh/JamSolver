7fe80738448d0b5c9f94eecd0845d4fe41ec52cc
ehehhh <ehehhh@gmail.com>
2016-05-01 18:05:37 +0300

    Version 2

--- JamSolver.java
+++ JamSolver.java
@@ -1,3 +1,5 @@
+package ee.subscribe;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -5,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Google Code Jam boilerplate reducer.
@@ -13,54 +16,51 @@ import java.nio.file.StandardOpenOption;
  */
 public class JamSolver {
 
+	private static final int VARYING_LINES_PER_TEST_CASE = -1;
+
 	private final int linesPerTestCase;
 	private final BufferedWriter writer;
 	private final BufferedReader reader;
+	private final boolean timeExecution;
 
 	private boolean started = false;
 
 	/**
-	 *
-	 * @param args				<code>args[0]</code> should be input file path, <code>args[1]</code> should be output file path
-	 * @param linesPerTestCase	number of lines in one test case
-	 * @return					JamSolver object (call .solve() on it)
+	 * @param args
+	 * 		<br />
+	 * 		&nbsp;&nbsp;&nbsp;&nbsp;<code>args[0]</code> - input file path<br />
+	 * 		&nbsp;&nbsp;&nbsp;&nbsp;<code>args[1]</code> - output file path
+	 * @return JamSolverRequiredFields object (next step in the builder)
 	 * @throws Throwable
 	 */
-	public static JamSolver with(String args[], int linesPerTestCase) throws Throwable {
+	public static JamSolverRequiredFields withIO(String args[]) throws Throwable {
 		if (args.length != 2) {
 			throw new Exception("args[0] should be the input file path, args[1] should be the output file path");
 		}
-		return new JamSolver(args, linesPerTestCase);
+		return new JamSolverRequiredFields(args[0], args[1]);
 	}
 
 	/**
-	 *
-	 * @param inPath			input file path
-	 * @param outPath			output file path
-	 * @param linesPerTestCase	number of lines in one test case
-	 * @return					JamSolver object (call .solve() on it)
+	 * @param inPath
+	 * 		input file path
+	 * @param outPath
+	 * 		output file path
+	 * @return JamSolverRequiredFields object (next step in the builder)
 	 * @throws Throwable
 	 */
-	public static JamSolver with(String inPath, String outPath, int linesPerTestCase) throws Throwable {
-		return new JamSolver(inPath, outPath, linesPerTestCase);
-	}
-
-	private JamSolver(String[] args, int linesPerTestCase) throws Throwable {
-		this(args[0], args[1], linesPerTestCase);
-	}
-
-	private JamSolver(String inFile, String outFile, int linesPerTestCase) throws Throwable {
-		if (linesPerTestCase < 1) {
-			throw new Exception("Lines per testcase has to be > 0");
+	public static JamSolverRequiredFields withIO(String inPath, String outPath) throws Throwable {
+		if (inPath == null || inPath.isEmpty()) {
+			throw new Exception("Parameter inPath cannot be null or empty");
 		}
+		if (outPath == null || outPath.isEmpty()) {
+			throw new Exception("Parameter outPath cannot be null or empty");
+		}
+		return new JamSolverRequiredFields(inPath, outPath);
+	}
+
+	private JamSolver(String inFile, String outFile, int linesPerTestCase, boolean timeExecution) throws Throwable {
+		this.timeExecution = timeExecution;
 		this.linesPerTestCase = linesPerTestCase;
-
-		if (inFile == null || inFile.length() == 0) {
-			throw new Exception("Input file path can't be empty!");
-		}
-		if (outFile == null || outFile.length() == 0) {
-			throw new Exception("Output file path can't be empty!");
-		}
 
 		Path inPath = Paths.get("", inFile);
 		if (Files.notExists(inPath)) {
@@ -78,41 +78,49 @@ public class JamSolver {
 		writer = Files.newBufferedWriter(outPath, StandardOpenOption.CREATE);
 	}
 
-	/**
-	 * Start solving the input file.
-	 * @param solver			a Solver interface with one method - solve(), which receives a String array of all the lines in a single test case<br />
-	 *                          and expects a String containing the output for the test case as a return value.<br />
-	 *                          NB! Don't write "Case #x: " in the return value, that is added automatically
-	 * @throws Throwable
-	 */
-	public void solve(Solver solver) throws Throwable {
+	void solve(Solver solver) throws Throwable {
 		if (started) {
 			throw new Exception("Solver has already been started once!");
+		} else {
+			started = true;
 		}
-		started = true;
-
+		long allTestCasesStartTime = System.nanoTime();
 		long numOfTestCases = Long.parseLong(reader.readLine());
-		int count = 0;
 		long testCaseCount = 0;
-		String line;
-		String[] testCase = new String[linesPerTestCase];
-		while ((line = reader.readLine()) != null) {
-			testCase[count] = line;
-			if (++count == linesPerTestCase) {
-				count = 0;
+		try {
+			while (testCaseCount < numOfTestCases) {
 				testCaseCount++;
+				int linesPerCase = linesPerTestCase != VARYING_LINES_PER_TEST_CASE ? linesPerTestCase : Integer.parseInt(reader.readLine());
+				String[] testCase = new String[linesPerCase];
+				int count = 0;
+				while (count < linesPerCase) {
+					String line = reader.readLine();
+					if (line != null) {
+						testCase[count++] = line;
+					} else {
+						throw new IOException("readLine() got null. Have you specified the correct lines per test case?");
+					}
+				}
+				long testCaseStartTime = System.nanoTime();
 				solveAndWrite(solver, testCase, testCaseCount, testCaseCount == numOfTestCases);
-				if (testCaseCount == numOfTestCases) {
-					break;
+				if (timeExecution) {
+					long testCaseTime = TimeUnit.MILLISECONDS.convert(System.nanoTime() - testCaseStartTime, TimeUnit.NANOSECONDS);
+					if (testCaseTime > 100) {
+						System.out.println("Test case #" + testCaseCount + " solved in " + getHumanReadableTimeFromMs(testCaseTime));
+					}
 				}
 			}
+			if (timeExecution) {
+				System.out.println("* All test cases executed in " + getHumanReadableTimeFromMs(TimeUnit.MILLISECONDS
+						.convert(System.nanoTime() - allTestCasesStartTime, TimeUnit.NANOSECONDS)) + " *");
+			}
+		} catch (Exception e) {
+			System.err.println("Error reading input file!");
+			e.printStackTrace();
 		}
 		reader.close();
 		writer.flush();
 		writer.close();
-		if (testCaseCount != numOfTestCases) {
-			throw new Exception("Did you enter the correct value for 'lines per test case'?");
-		}
 	}
 
 	private void solveAndWrite(Solver solver, String[] testCase, long testCaseCount, boolean isLast) throws Throwable {
@@ -122,12 +130,123 @@ public class JamSolver {
 		}
 	}
 
+	private String getHumanReadableTimeFromMs(long timeInMs) {
+		if (timeInMs > 10000) {
+			return "~" + TimeUnit.SECONDS.convert(timeInMs, TimeUnit.MILLISECONDS) + " seconds";
+		} else {
+			return timeInMs + " milliseconds";
+		}
+	}
+
+	public static class JamSolverRequiredFields {
+
+		private final String inFile;
+		private final String outFile;
+
+		private JamSolverRequiredFields(String inFile, String outFile) {
+			this.inFile = inFile;
+			this.outFile = outFile;
+		}
+
+		/**
+		 * When all of the test cases have a constant number of lines, this method should be used.
+		 * <br /><br />
+		 * <i>Example input file with 2 lines per test case:</i><br />
+		 * <code>
+		 * 3<br />
+		 * testcase1_line1<br />
+		 * testcase1_line2<br />
+		 * testcase2_line1<br />
+		 * testcase2_line2<br />
+		 * testcase3_line1<br />
+		 * testcase3_line2<br />
+		 * </code>
+		 *
+		 * @param linesPerTestCase
+		 * 		lines per test case has to be >= 1
+		 * @return JamSolverOptionalFields builder object
+		 * @throws Throwable
+		 */
+		public JamSolverOptionalFields withLinesPerTestCase(int linesPerTestCase) throws Throwable {
+			if (linesPerTestCase < 1) {
+				throw new Exception("Lines per test case must be >=1");
+			}
+			return new JamSolverOptionalFields(inFile, outFile, linesPerTestCase);
+		}
+
+		/**
+		 * When the test cases have a varying number of lines, this method should be used.<br /><br />
+		 * <i>Example input file with varying lines per test case:</i><br />
+		 * <code>
+		 * 3<br />
+		 * 2<br />
+		 * testcase1_line1<br />
+		 * testcase1_line2<br />
+		 * 1<br />
+		 * testcase2_line1<br />
+		 * 4<br />
+		 * testcase3_line1<br />
+		 * testcase3_line2<br />
+		 * testcase3_line3<br />
+		 * testcase3_line4
+		 * </code>
+		 *
+		 * @return JamSolverOptionalFields builder object
+		 */
+		public JamSolverOptionalFields withVaryingLinesPerTestCase() {
+			return new JamSolverOptionalFields(inFile, outFile, VARYING_LINES_PER_TEST_CASE);
+		}
+	}
+
+	public static class JamSolverOptionalFields {
+
+		private final String inFile;
+		private final String outFile;
+		private final int linesPerTestCase;
+		private boolean timeExecution = false;
+
+		private JamSolverOptionalFields(String inFile, String outFile, int linesPerTestCase) {
+			this.inFile = inFile;
+			this.outFile = outFile;
+			this.linesPerTestCase = linesPerTestCase;
+		}
+
+		/**
+		 * Enabling execution time logging means that:<br />
+		 * &nbsp;&nbsp;&nbsp;&nbsp;a) if the execution time for a single test case exceeds 100ms,
+		 * it gets logged to stdout<br />
+		 * &nbsp;&nbsp;&nbsp;&nbsp;b) when all test cases have been executed,
+		 * a total execution time gets logged to stdout
+		 *
+		 * @return JamSolverOptionalFields builder object
+		 */
+		public JamSolverOptionalFields enableExecutionTimeLogging() {
+			timeExecution = true;
+			return this;
+		}
+
+		/**
+		 * Start solving the input file.
+		 *
+		 * @param solver
+		 * 		a Solver interface with one method - <code>solve()</code>,
+		 * 		which receives a String array of all the lines in a single test case<br />
+		 * 		and expects a String containing the output for the test case as a return value.<br /><br />
+		 * 		<b>NB!</b> Don't write "Case #x: " in the return value, that is added automatically
+		 * @throws Throwable
+		 */
+		public void solve(Solver solver) throws Throwable {
+			new JamSolver(inFile, outFile, linesPerTestCase, timeExecution)
+					.solve(solver);
+		}
+	}
+
 	public interface Solver {
 		/**
-		 *
-		 * @param testCase		all the lines in a single test case as a String array
-		 * @return				output for said test case
+		 * @param testCase
+		 * 		all the lines in a single test case as a String array
+		 * @return output for said test case
 		 */
-		String solve(String[] testCase);
+		String solve(String[] testCase) throws Throwable;
 	}
 }
--- README.md
+++ README.md
@@ -5,42 +5,60 @@ A Google CodeJam boilerplate reducer, allowing you to concentrate on the real pr
 __At least Java 7 is required to use this.__
 
 ```java
-class Main {
+public class Main {
 
-  public static void main(String args[]) {
-    final String inPath = "C:\Code\Jam\input.in";
-    final String outPath = "C:\Code\Jam\output.out";
-    final int linesPerTestCase = 3;
+ public static void main(String[] args) throws Throwable {
+  final String inPath = "C:\\path\\to\\input.in";
+  final String outPath = "C:\\path\\to\\output.out";
 
-	// Java 7 example
-    JamSolver.with(inPath, outPath, linesPerTestCase)
-      .solve(new JamSolver.Solver() {
-        @Override
-        public String solve(String[] testCase) {
-          // testCase is a String array with length specified by linesPerTestCase
-          // TODO solve the test case
-          return "solution_to_test_case";
-        }
-      });
+  /**
+   * Fixed lines per test case example.
+   *
+   * Input file with 2 lines per test case:
+   * 3
+   * testcase1_line1
+   * testcase1_line2
+   * testcase2_line1
+   * testcase2_line2
+   * testcase3_line1
+   * testcase3_line2
+   */
+  JamSolver.withIO(inPath, outPath)
+    .withLinesPerTestCase(2)
+    .enableExecutionTimeLogging()    // You can choose to time the execution of your solution.
+    .solve(testCase -> {
+     // testCase is a String array with length specified by linesPerTestCase
+     // TODO solve the test case
+     return "solution_to_test_case";
+    });
 
-    // Looks a lot better with Java8+ lambdas
-    JamSolver.with(inPath, outPath, linesPerTestCase)
-      .solve(testCase -> {
-        // TODO solve the test case
-        return "solution_to_test_case";
-      });
-  
-    // Input and output file paths can also be passed in as parameters,
-    // where args[0] is the input and args[1] is the output
-    JamSolver.with(args, linesPerTestCase)
-      .solve(testCase -> {
-        // TODO solve the test case
-        return "solution_to_test_case";
-      });
-  }
+  /**
+   * Varying lines per test case example.
+   *
+   * Input file with varying lines per test case:
+   * 3
+   * 2
+   * testcase1_line1
+   * testcase1_line2
+   * 1
+   * testcase2_line1
+   * 4
+   * testcase3_line1
+   * testcase3_line2
+   * testcase3_line3
+   * testcase3_line4
+   */
+  JamSolver.withIO(inPath, outPath)
+    .withVaryingLinesPerTestCase()
+    .solve(testCase -> {
+     // testCase is a String array with length that varies for each test case
+     // TODO solve the test case
+     return "solution_to_test_case";
+    });
+ }
 }
 ```
 
 Usage
 -----
-Just download JamSolver.java, add it to your project and you're good to go.
+Just copy JamSolver.java into your project and you're good to go.
